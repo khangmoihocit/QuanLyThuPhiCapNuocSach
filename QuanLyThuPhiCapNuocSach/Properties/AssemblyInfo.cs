f43df3392739e830efc7563440ee1765163f73f3
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-02-27 13:28:00 +0700

    Add project files.

---
+++ QuanLyThuPhiCapNuocSach/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QuanLyThuPhiCapNuocSach")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuanLyThuPhiCapNuocSach")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b31e28ff-22ff-4728-b4fa-012805f07d93")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
